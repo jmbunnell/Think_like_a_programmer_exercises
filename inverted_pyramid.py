b9a83bf858aea0ffec2b30fc9207ba6ad54ea1c8
JB <jacobbunnellca@gmail.com>
2022-10-22 12:23:29 -0400

    Prints inverted pyramid given input from 1-8

--- inverted_pyramid.py
+++ inverted_pyramid.py
@@ -8,10 +8,15 @@
 # Base of 8; minus 2 hashes and add 1 space each side per line; end with 2 hashes - height: 4 lines & width: 8 characters
 
 # Get integer input from user
-input = int(input("Height: "))
-print(f"{input}")
+while True:
+    prompt = int(input("Height: "))
+    if prompt > 0 and prompt < 9:
+        break
 
-# Print out 8 hashmarks on same line
-first_value = input * 2
-for i in range(input):
-   print('#' * first_value)
+
+# Print out hash marks and spaces
+row_count = prompt * 2
+for row in range(1, prompt + 1):
+    print(" " * (row - 1) + "#" * (row_count) + " " * (row - 1))
+    row_count = row_count - 2
+    
