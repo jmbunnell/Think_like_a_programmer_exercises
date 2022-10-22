0d123277ff9d2abb8b4a538b293edfcf789603b5
JB <jacobbunnellca@gmail.com>
2022-10-22 12:05:08 -0400

    Prints first line of hash marks

--- inverted_pyramid.py
+++ inverted_pyramid.py
@@ -5,4 +5,13 @@
   ####
    ##
 
-# Base of 8; minus 2 hashes and add 1 space each side per line; end with 2 hashes
+# Base of 8; minus 2 hashes and add 1 space each side per line; end with 2 hashes - height: 4 lines & width: 8 characters
+
+# Get integer input from user
+input = int(input("Height: "))
+print(f"{input}")
+
+# Print out 8 hashmarks on same line
+first_value = input * 2
+for i in range(input):
+   print('#' * first_value)
